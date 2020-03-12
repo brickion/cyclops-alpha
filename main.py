445632b1bbab3bb5ca05a3e906cb258f630989a0
Roy Hui <roy@brickion.com>
2020-03-12 16:48:55 +1100

    nvidia jetson play

--- main.py
+++ main.py
@@ -3,11 +3,40 @@ import cv2
 import numpy as np
 import config
 import time, math
+import platform
 from datetime import datetime, timedelta
+
+def running_on_jetson_nano():
+    # To make the same code work on a laptop or on a Jetson Nano, we'll detect when we are running on the Nano
+    # so that we can access the camera correctly in that case.
+    # On a normal Intel laptop, platform.machine() will be "x86_64" instead of "aarch64"
+    return platform.machine() == "aarch64"
+
+
+def get_jetson_gstreamer_source(capture_width=1280, capture_height=720, display_width=640, display_height=360, framerate=30, flip_method=0):
+    """
+    Return an OpenCV-compatible video source description that uses gstreamer to capture video from the camera on a Jetson Nano
+    """
+    return (
+            f'nvarguscamerasrc ! video/x-raw(memory:NVMM), ' +
+            f'width=(int){capture_width}, height=(int){capture_height}, ' +
+            f'format=(string)NV12, framerate=(fraction){framerate}/1 ! ' +
+            f'nvvidconv flip-method={flip_method} ! ' +
+            f'video/x-raw, width=(int){display_width}, height=(int){display_height}, format=(string)BGRx ! ' +
+            'videoconvert ! video/x-raw, format=(string)BGR ! appsink'
+            )
+
 
 NEW_EVENT_MINS = 5
 
-video_capture = cv2.VideoCapture(0)
+if running_on_jetson_nano():
+    # Accessing the camera with OpenCV on a Jetson Nano requires gstreamer with a custom gstreamer source string
+    video_capture = cv2.VideoCapture(get_jetson_gstreamer_source(), cv2.CAP_GSTREAMER)
+else:
+    # Accessing the camera with OpenCV on a laptop just requires passing in the number of the webcam (usually 0)
+    # Note: You can pass in a filename instead if you want to process a video file instead of a live camera stream
+    video_capture = cv2.VideoCapture(0)
+
 video_capture.set(cv2.CAP_PROP_FRAME_WIDTH, 400)
 video_capture.set(cv2.CAP_PROP_FRAME_HEIGHT, 300)
 
